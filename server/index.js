4b2db317c59bc2a327add72c1bac88fc267db398
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-24 11:03:03 -0700

    set up server.js

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Promise = require('bluebird');
+//const db = path
+
+const PORT = process.env.PORT || 3000;
+const PUBLIC_DIR = path.resolve(__dirname, '../public');
+
+app.use(bodyParser.json());
+app.use(express.static(PUBLIC_DIR));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
