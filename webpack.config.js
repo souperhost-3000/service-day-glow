aeb799b1c4115ff5f7b22e40b89e549b3953e14f
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-24 14:12:48 -0700

    added webpack.config.js setup

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const SRC_FILE = path.resolve(__dirname, 'client', 'src', 'index.js');
+const OUT_DIR = path.resolve(__dirname, 'public');
+
+
+module.exports = {
+  entry: SRC_FILE,
+  output: {
+    path: OUT_DIR,
+    filename: 'bundle.js'
+  }
+}
