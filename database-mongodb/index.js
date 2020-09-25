5a43715d9ebb521195e2f660fded55205cb49a3e
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-24 17:06:18 -0700

    combined index.js and Lisitng files into one in db folder, added seeding psuedo

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -1,10 +1,27 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost/27017/';
+const mongoURI = 'mongodb://localhost/27017';
 
 const db = mongoose.connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log('Connected to mongoDB and mongoose'))
   .catch((err) => console.log(err));
 
-module.exports = db;
+mongoose.Promise = global.Promise;
+
+const listingSchema = new mongoose.Schema({
+  listing_id: Number,
+  price: Number,
+  rating: Number,
+  reviews_count: Number,
+  guest_limit: Number,
+  guest_included: Number,
+  guest_extra_charge: Number,
+  cleaning_fee: Number,
+  service_fee: Number,
+  taxes: Number,
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+module.exports = { db, Listing };
---
+++ database-mongodb/seed.js
@@ -0,0 +1,8 @@
+const { Listing } = require('./index');
+
+// create seeding logic to create new documents in db collection
+// create math.random func to use for random number selector
+// use schema to fill nums into each key:value pair
+// once an individual document has all values, invoke .create
+// use a promise .then, .catch
+// repeat with loop until 100 documents have been created
