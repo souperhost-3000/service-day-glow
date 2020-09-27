269db0e844f68ae61ddd231da194a640fad26ef8
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-27 10:16:27 -0700

    installed style loader & webpack script

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
