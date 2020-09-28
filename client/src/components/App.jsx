355d7b215c4cd6ff934cfc1295acbd3e357b34a2
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-28 12:32:58 -0700

    app pseudo code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import '../styles/app.css';
 
+/* App -> contains:
+price (upper left)
+rating (upper right)
+date picker (mid-upper)
+guestsDetails(mid-lower)
+Check Avail button (lower)
+(hidden) - cost estimate & reserve button
+
+modals (calendar pop up, guest picker drop down, check avail (expands app container))
+
+state:
+  manage property details in props
+
+render each nested functional component (use hooks if needed)
+*/
+
 // top level service component (displayed before user interacts with anything)
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      // hard code some data to dummy display
     };
   }
+
+  // Axios GET req
+
+  // add price and reviews to app level
 
   render() {
     return (
