6aac4e24f1337e4408cd6c8cc9e6d280283b24f1
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-29 10:37:34 -0700

    connected cal modal component with cond. rendering

--- client/src/components/Modals/CalModal.jsx
+++ client/src/components/Modals/CalModal.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 
 // Calendar modal (conditional pop up logic)
-function CalModal() {
+function CalModal({ show }) {
+  if (!show) {
+    return null;
+  }
   return (
-    <div />
+    <div>
+      {console.log('modal has appeared')}
+      <div>Hello Cal Modal</div>
+    </div>
     // reusable calendar func imported
   );
 }
