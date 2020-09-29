47b5dc2232f431ea174a98deecc2dcfb2b908329
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-29 12:34:24 -0700

    modal CSS (in progress - currently visible in front of components)

--- client/src/components/DatePicker.jsx
+++ client/src/components/DatePicker.jsx
@@ -6,27 +6,33 @@ function DatePicker({ availability }) {
   const [showCalModal, setCalModal] = useState(false);
 
   useEffect(() => {
-    console.log("showCalModal is: ",showCalModal);
+    console.log('showCalModal is: ', showCalModal);
   }, [showCalModal]);
 
   return (
-    <div className="datePicker">
-      <span className="check-in-app">
-        Check-in
-        <form className="date-form">
-          <input id="check-in-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
-        </form>
-      </span>
-      <span className="check-out-app">
-        Checkout
-        <form className="date-form">
-          <input id="check-out-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
-        </form>
-      </span>
-      <CalModal
-        show={showCalModal}
-        setCalModal={setCalModal}
-      />
+    <div>
+      <div className="datePicker">
+        <span className="check-in-app">
+          Check-in
+          <form className="date-form">
+            <input id="check-in-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
+          </form>
+        </span>
+        <span className="check-out-app">
+          Checkout
+          <form className="date-form">
+            <input id="check-out-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
+          </form>
+        </span>
+      </div>
+      <div className="modal">
+        <span>
+          <CalModal
+            show={showCalModal}
+            setCalModal={setCalModal}
+          />
+        </span>
+      </div>
     </div>
   );
 }
