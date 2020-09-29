51afc820b83d3ff3d54e45db56f7c5ad83bfbfce
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-29 14:25:59 -0700

    CSS for check in/out input box

--- client/src/components/DatePicker.jsx
+++ client/src/components/DatePicker.jsx
@@ -15,13 +15,13 @@ function DatePicker({ availability }) {
         <span className="check-in-app">
           Check-in
           <form className="date-form">
-            <input id="check-in-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
+            <input className="date-input" id="check-in-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
           </form>
         </span>
         <span className="check-out-app">
           Checkout
           <form className="date-form">
-            <input id="check-out-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
+            <input className="date-input" id="check-out-input" type="text" placeholder="Add date" onClick={() => setCalModal(true)} />
           </form>
         </span>
       </div>
