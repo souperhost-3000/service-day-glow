8d75bca91ea5063f488daf4378ab4414071cbdcc
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-28 20:35:54 -0700

    commented out tests

--- client/src/components/Guests.jsx
+++ client/src/components/Guests.jsx
@@ -7,7 +7,7 @@ function Guests() {
       <span id="guest-title">Guests</span>
       <br />
       <span id="guestCount"># guests</span>
-      <button id="guest-expand-mod"> ╲╱</button>
+      <button id="guest-expand-mod"> ╲╱ </button>
       {/* <button id="guest-collapse-mod"> ╱╲</button> */}
     </div>
   );
