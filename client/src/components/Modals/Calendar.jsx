24270ae77a1b73c65a6c6c14eef3416ee6be4152
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-09-29 20:24:58 -0700

    calendar hard-coded frame

--- client/src/components/Modals/Calendar.jsx
+++ client/src/components/Modals/Calendar.jsx
@@ -1,11 +1,32 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 
 // Calendar (reuseable)
 function Calendar() {
+  const nums = Array(42).fill(null);
+  for (let i = 0; i < nums.length; i += 1) {
+    nums[i] = <button className="day" type="button" key={i}>{i}</button>;
+  }
+  console.log(nums);
+
   return (
-    <div>
-      <div>
-        Dynamic Calendar goes here
+    <div className="cal-container">
+      <div className="month">
+        Month Name
+      </div>
+      <div className="cal-container">
+        <div className="weekdays">
+          <button className="cal-day" type="button">Su</button>
+          <button className="cal-day" type="button">Mo</button>
+          <button className="cal-day" type="button">Tu</button>
+          <button className="cal-day" type="button">We</button>
+          <button className="cal-day" type="button">Th</button>
+          <button className="cal-day" type="button">Fr</button>
+          <button className="cal-day" type="button">Sa</button>
+        </div>
+        <div className="days-container">
+          {nums}
+        </div>
       </div>
     </div>
   );
